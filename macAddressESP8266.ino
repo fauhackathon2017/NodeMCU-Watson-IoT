cf4af6154f673932c027dd7feb90c290597cc3eb
Andrew Elek <ajelek@us.ibm.com>
2017-02-09 16:34:03 -0500

    deleted extra header files

--- macAddressESP8266.ino
+++ macAddressESP8266.ino
@@ -3,16 +3,6 @@
  */
 
 #include <ESP8266WiFi.h>
-#include <ESP8266WiFiAP.h>
-#include <ESP8266WiFiGeneric.h>
-#include <ESP8266WiFiMulti.h>
-#include <ESP8266WiFiScan.h>
-#include <ESP8266WiFiSTA.h>
-#include <ESP8266WiFiType.h>
-#include <WiFiClient.h>
-#include <WiFiClientSecure.h>
-#include <WiFiServer.h>
-#include <WiFiUdp.h>
 
 int completionStatus=0;
 void setup() {
